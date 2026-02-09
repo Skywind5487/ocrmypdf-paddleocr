1a1f54648daa3df061ba3e7d1d7875d035e9e041
Skywind5487 <skywind5487@gmail.com>
2026-02-09 16:58:22 +0800

    Fix hOCR lang attribute to use selected language

--- src/ocrmypdf_paddleocr/plugin.py
+++ src/ocrmypdf_paddleocr/plugin.py
@@ -240,7 +240,7 @@ class PaddleOCREngine(OcrEngine):
             '<?xml version="1.0" encoding="UTF-8"?>',
             '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"',
             '    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
-            '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">',
+            f'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="{hocr_lang}" lang="{hocr_lang}">',
             '<head>',
             '<title></title>',
             '<meta http-equiv="content-type" content="text/html; charset=utf-8" />',
